46e21a5148cbd760d221eb0b71402d161cb51701
Hiep Nguyen <nguyenhieptech@gmail.com>
2023-09-08 01:52:42 +0700

    chore: rename App.tsx to app.tsx (case sensitive)

---
+++ src/app.tsx
@@ -0,0 +1,12 @@
+import { HelloWorld } from '@/components/hello-world';
+
+function App() {
+  return (
+    <div>
+      Hello brand new React Query Example project
+      <HelloWorld />
+    </div>
+  );
+}
+
+export default App;
